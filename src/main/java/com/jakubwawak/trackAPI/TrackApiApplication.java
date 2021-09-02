bace32d8dc320bf93e721a68c8d02519fd8a903e
Jakub Wawak <34348257+wjakew@users.noreply.github.com>
2021-09-02 23:53:13 +0200

    Initial Commit

---
+++ src/main/java/com/jakubwawak/trackAPI/TrackApiApplication.java
@@ -0,0 +1,14 @@
+package com.jakubwawak.trackAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackApiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("WITAJ");
+		SpringApplication.run(TrackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakubwawak/trackAPI/TrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubwawak.trackAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
